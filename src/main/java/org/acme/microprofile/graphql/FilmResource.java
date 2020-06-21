c4bb4552492302b1e585245dd761fd3ac0609966
gbengataylor <gb@redhat.com>
2020-06-21 16:48:12 -0400

    update

--- src/main/java/org/acme/microprofile/graphql/FilmResource.java
+++ src/main/java/org/acme/microprofile/graphql/FilmResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import org.eclipse.microprofile.graphql.DefaultValue;
 import org.eclipse.microprofile.graphql.GraphQLApi;
 import org.eclipse.microprofile.graphql.Mutation;
 import org.eclipse.microprofile.graphql.Name;
@@ -19,6 +20,8 @@ public class FilmResource {
     @Inject
     GalaxyService service;
 
+    // queries
+
     @Query("allFilms") 
     @Description("Get all Films from a galaxy far far away") 
     public List<Film> getAllFilms() {
@@ -30,11 +33,12 @@ public class FilmResource {
     public Film getFilm(@Name("filmId") int id) {
         return service.getFilm(id);
     } 
+    // this is not a "hero" query however will update the film schema to include associated heroes
     public List<Hero> heroes(@Source Film film) { 
         return service.getHeroesByFilm(film);
     }
 
-
+// mutations
     @Mutation
     public Hero createHero(Hero hero) {
         service.addHero(hero);
@@ -44,5 +48,12 @@ public class FilmResource {
     @Mutation
     public Hero deleteHero(int id) {
         return service.deleteHero(id);
+    }
+
+    // queries by field
+    // note this is a hero query
+    @Query
+    public List<Hero> getHeroesWithSurname(@DefaultValue("Skywalker") String surname) {
+        return service.getHeroesBySurname(surname);
     }
 }
