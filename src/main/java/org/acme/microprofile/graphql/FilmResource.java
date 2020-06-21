caa6763f233793d330a4f2573b4ae22bccd4f681
gbengataylor <gb@redhat.com>
2020-06-21 16:39:33 -0400

    first commit

---
+++ src/main/java/org/acme/microprofile/graphql/FilmResource.java
@@ -0,0 +1,48 @@
+package org.acme.microprofile.graphql;
+
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.eclipse.microprofile.graphql.GraphQLApi;
+import org.eclipse.microprofile.graphql.Mutation;
+import org.eclipse.microprofile.graphql.Name;
+import org.eclipse.microprofile.graphql.Query;
+import org.eclipse.microprofile.graphql.Source;
+
+import io.vertx.core.cli.annotations.Description;
+
+@GraphQLApi
+public class FilmResource {
+
+    @Inject
+    GalaxyService service;
+
+    @Query("allFilms") 
+    @Description("Get all Films from a galaxy far far away") 
+    public List<Film> getAllFilms() {
+        return service.getAllFilms();
+    }
+
+    @Query
+    @Description("Get a Films from a galaxy far far away")
+    public Film getFilm(@Name("filmId") int id) {
+        return service.getFilm(id);
+    } 
+    public List<Hero> heroes(@Source Film film) { 
+        return service.getHeroesByFilm(film);
+    }
+
+
+    @Mutation
+    public Hero createHero(Hero hero) {
+        service.addHero(hero);
+        return hero;
+    }
+
+    @Mutation
+    public Hero deleteHero(int id) {
+        return service.deleteHero(id);
+    }
+}
